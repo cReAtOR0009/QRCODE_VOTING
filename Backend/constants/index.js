6aa249c8b3a9e3bebae46d4880aa63b3052dac53
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-02-13 16:36:56 +0100

    added custom default server responses

---
+++ Backend/constants/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    defaultServerResponse: {
+        status: 400,
+        message: '',
+        data: {}
+    },
+
+    voteResponse:{
+        usedQRCode:"this QR Code has been used before",
+        wrongQRCode:"this QR Code dosent exist, kidly visit our stand to get a valid one",
+        successfull:"voting SuccesFull",
+        unSuccessfull:"voting unSuccessful"
+    }
+}
