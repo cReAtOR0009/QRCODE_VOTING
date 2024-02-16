5da581e1ddc8753b32acb291e239addf416a77ec
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-02-16 11:08:18 +0100

    modifying ui

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -3,12 +3,20 @@ import { styles } from "../styles/styles";
 
 const Header = () => {
   return (
-    <div className="container  relative flex sm:flex-nowrap flex-wrap  gap-[20px]  mt-[70px] sm:mt-[100px] ">
-      <section className="mt-[30px]  w-100">
-        <div className="headerTextContainer ">
-          <h1 className={`${styles.heading} px-[10px] text-left`}>
+    <div className="container  relative flex  sm:flex-nowrap flex-wrap  gap-[20px]  mt-[70px] sm:mt-[100px] ">
+      <section className="mt-[30px]  w-100 h-[80vh]">
+        <div className="">
+          <h1 className={`${styles.heading}  text-left`}>
             Loveworld, giving your Life a Meaning...
           </h1>
+        </div>
+        <div className="py-[20px] text-[16px] sm:text-[22px] text-left">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur
+          repudiandae, ducimus aliquid delectus eos quisquam. Eum quisquam in
+          sed totam quos asperiores accusamus saepe, placeat cum cupiditate
+          molestiae fugiat sunt corrupti. Omnis recusandae ullam aliquam
+          laudantium, repellat deserunt voluptatum necessitatibus minima error,
+          rem nostrum deleniti neque tempore officiis, iusto perspiciatis!
         </div>
         <div className="flex  flex-col sm:flex-row justify-end text-[right] my-[10px]">
           <button
--- Frontend/src/components/generateQR.jsx
+++ Frontend/src/components/generateQR.jsx
@@ -17,6 +17,7 @@ const GenerateQR = ({ value, index }) => {
           style={{ height: "auto", maxWidth: "100%", width: "100%" }}
           value={value}
           viewBox={`0 0 256 256`}
+          
         />
       </div>
     </div>
