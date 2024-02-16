6ce7ececb1ae5115d601fa3357bccd10e2661c22
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-02-16 11:12:44 +0100

    ui changes

--- Frontend/src/context/formContext.jsx
+++ Frontend/src/context/formContext.jsx
@@ -5,8 +5,7 @@ import { qrCodeScannerContext } from "./qrCodeScannerContext";
 export const FormContext = createContext();
 
 export const FormProvider = ({ children }) => {
-  const { scanResult, setScanResult, formData, setFormData, handleSubmit, vote, handleChange } = useContext(qrCodeScannerContext);
-  console.log("logging scan Result from form context", scanResult);
+  const { formData, setFormData, handleSubmit, vote, handleChange } = useContext(qrCodeScannerContext);
  
   return (
     <FormContext.Provider
