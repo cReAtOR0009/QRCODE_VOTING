1c90eb72faf74ee5f08465543f89f22bb08cd939
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-02-17 07:21:21 +0100

    removed unwanted files and qrcode

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import Scanner from "../components/Scanner";
-import GenerateQR from "../components/generateQR";
 import VoterForm from "../components/VoterForm.jsx";
-//C:\WEB-DEVELOPMENT\QRCODE_VOTING\Frontend\src\components\VoteForm.jsx
-//Frontend\src\components\VoteForm.jsx
-import NavBar from "../components/NavBar";
 import Header from "../components/Header";
-import { urls } from "../qrCodes";
 
 const HomePage = () => {
   return (
@@ -15,11 +10,6 @@ const HomePage = () => {
       <div>
         <VoterForm />
         <Scanner />
-        <div className="container qrcodeContainer">
-          {urls.map((url, index) => (
-            <GenerateQR key={index} value={url.url} index={index} />
-          ))}
-        </div>
       </div>
     </>
   );
